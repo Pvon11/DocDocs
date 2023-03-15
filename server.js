89b6126091740d482acb674c1f45ea843444519c
ELENA VALEEVA <elenavaleeva05@gmail.com>
2023-03-15 18:57:07 -0400

    adding seeds

--- server.js
+++ server.js
@@ -37,3 +37,38 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+
+// const express = require('express');
+// const path = require('path');
+
+
+app.get('/invoice/:invoiceId', (req, res) => {
+
+  res.send({ data: 'invoiceBtn' })
+});
+
+app.post('/invoice/:invoiceId', (req, res) => {
+  res.send({ data: 'invoice' })
+});
+
+app.put('/invoice/:invoiceId', (req, res) => {
+  res.send({ data: 'invoice' })
+});
+
+app.delete('/invoice/:invoiceId', (req, res) => {
+  res.send({ data: 'invoice' })
+});
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+
+
+app.listen(PORT, () => console.log('Now listening'));
